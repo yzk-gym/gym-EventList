28ed7c2b0969c748056fe38c4f0110f57a12821e
yuya <siren.night43@gmail.com>
2018-07-21 17:51:02 +0900

    vie

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,11 +10,10 @@ class EventsController < ApplicationController
 
   def client
       client = Twitter::REST::Client.new do |config|
-        config.consumer_key = "Pg1MaqymBgzXxtL1r6JWGabCW"
-        config.consumer_secret = "EsVuRYP6mkXL7hO6eqfFouayvEEhESen7EwdLf8RoNANJv4E40"
-        config.access_token = "920764129-hc4q82PJDunzv4pbU6OMoSw96iPRBubDqCtfg45P"
-        config.access_token_secret = "pF4ihD1GuC08fKiLLZ40cIRkV9d6ufGPYESU7SsF6lVkr"
-    end
+        config.consumer_key = Rails.application.secrets.user_consumer_key
+        config.consumer_secret = Rails.application.secrets.consumer_secret
+        config.access_token = Rails.application.secrets.access_token
+        config.access_token_secret = Rails.application.secrets.access_token_secret
+      end
   end
-
 end
