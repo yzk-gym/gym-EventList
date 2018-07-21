b70c340a95e3b19534c612571373478dfd52d951
yuya <siren.night43@gmail.com>
2018-07-21 17:32:44 +0900

    twitterAPI導入

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,20 @@
 class EventsController < ApplicationController
+  before_action :client
+
+  def test
+    @tweet = client.user_timeline("nanami_3615")
+  end
+
+
+  private
+
+  def client
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key = "Pg1MaqymBgzXxtL1r6JWGabCW"
+        config.consumer_secret = "EsVuRYP6mkXL7hO6eqfFouayvEEhESen7EwdLf8RoNANJv4E40"
+        config.access_token = "920764129-hc4q82PJDunzv4pbU6OMoSw96iPRBubDqCtfg45P"
+        config.access_token_secret = "pF4ihD1GuC08fKiLLZ40cIRkV9d6ufGPYESU7SsF6lVkr"
+    end
+  end
+
 end
